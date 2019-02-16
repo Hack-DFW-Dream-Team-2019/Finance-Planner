0ea27c23bc131ab69c6da470068b8a870d322806
Caleb Trachte <cetrachte@hotmail.com>
2019-02-16 16:09:24 -0600

    adjust api call to include correct params and headers

--- app/src/helpers/occupationsApi.js
+++ app/src/helpers/occupationsApi.js
@@ -1,18 +1,21 @@
 import Axios from 'Axios';
 
-function getOccupationSalary(occupation_code) {
-  Axios.get('https://api.bls.gov/publicAPI/v1/timeseries/data/',
+function getOccupationSalary(occupation_code, year="2018") {
+  Axios.get('https://api.bls.gov/publicAPI/v1/timeseries/data/LAUCN040010000000005',
     {
       params: {
-        series_id: 'LAUCN040010000000005',
-        occupation_code: occupation_code
+        occupation_code: occupation_code,
+        startyear:year,
+        endyear:year
       }
     })
     .then(function (response) {
       console.log(response);
+      return response;
     })
     .catch(function (error) {
       console.log(error);
+      return error;
     });
 };
 
