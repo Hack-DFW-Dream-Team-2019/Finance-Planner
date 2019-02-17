e5530bece0bfd55fef3d6e9e4c0c24de21ed3df4
Caleb Trachte <cetrachte@hotmail.com>
2019-02-16 22:16:32 -0600

    fix merge conflict

--- app/src/views/Education.vue
+++ app/src/views/Education.vue
@@ -1,28 +1,8 @@
 <template>
     <div>
-<<<<<<< HEAD
         <p>Hello this is the education screen</p>
 
         <button type="button" @click="testing()" name="button"></button>
-||||||| merged common ancestors
-        <p>Hello this is the education screen</p>
-=======
-        <div class="education-side-nav">
- 			<h5 class="education-title">Education</h5>
-            <router-link to="/education/introduction">
-				<p>Introduction</p>
-			</router-link>			
-			<router-link to="/education/finance">
-				<p>Finance 101</p>
-			</router-link>
-            <router-link to="/education/loans">
-				<p>Loan Options</p>
-			</router-link>
-        </div>
-        <div class="content">
-            <router-view></router-view>
-        </div>
->>>>>>> 3b9c0e04f3878a5faa2cc85281871cecd541adfc
     </div>
 </template>
 
@@ -65,6 +45,6 @@ export default {
 		color: white;
 		margin-bottom: 75px;
 	}
-	
-	
+
+
 </style>
