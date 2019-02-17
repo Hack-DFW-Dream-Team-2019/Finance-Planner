b4e78cf226f771014e50467593cb67e9da5c65d6
Caleb Trachte <cetrachte@hotmail.com>
2019-02-16 18:26:28 -0600

    remove reference to fortawesome

--- app/src/main.js
+++ app/src/main.js
@@ -3,11 +3,6 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-
-library.add(faCoffee)
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
